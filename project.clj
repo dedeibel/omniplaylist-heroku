8be11a43a68c852132392f106313800c9f2bca0c
Benjamin Peter <benjaminpeter@arcor.de>
2013-06-30 18:10:29 +0200

    local dependency resolution: another try with io file.

--- project.clj
+++ project.clj
@@ -20,5 +20,5 @@
   :min-lein-version "2.0.0"
   :plugins [[environ/environ.lein "0.2.1"]]
   :hooks [environ.leiningen.hooks]
-  :repositories {"local" "file://repo"}
+  :repositories { "local" ~(str (.toURI (java.io.File. "repo"))) }
   :profiles {:production {:env {:production true}}})
